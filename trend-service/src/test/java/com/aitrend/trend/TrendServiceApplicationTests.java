3933a47b7791d2a8beddf407592612da46c23377
Akshay Dhobale <akshaysd592@gamil.com>
2026-08-22 10:25:48 +0530

    test: update H2 test properties for TrendServiceApplicationTests

--- trend-service/src/test/java/com/aitrend/trend/TrendServiceApplicationTests.java
+++ trend-service/src/test/java/com/aitrend/trend/TrendServiceApplicationTests.java
@@ -6,11 +6,12 @@ import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(properties = {
-    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=PostgreSQL",
+    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE",
     "spring.datasource.driver-class-name=org.h2.Driver",
     "spring.datasource.username=sa",
     "spring.datasource.password=",
-    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.liquibase.enabled=true"
 })
 class TrendServiceApplicationTests {
 
