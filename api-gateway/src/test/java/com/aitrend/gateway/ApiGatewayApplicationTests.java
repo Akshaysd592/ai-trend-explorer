2a41d0c8360c9f718018c6a82b077f32934ff5e9
Akshay Dhobale <akshaysd592@gamil.com>
2026-09-19 09:45:03 +0530

    refactor: remove redundant post-processor workaround files while preserving render.yamlIaC config

---
+++ api-gateway/src/test/java/com/aitrend/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aitrend.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Verifies Spring Cloud Gateway application context starts up cleanly
+    }
+}
